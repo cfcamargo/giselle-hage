4ae98a1c23d9029d6caa93fdaead2a98ae19a79e
cristian <contato@camargodev.com.br>
2023-08-25 15:52:32 -0400

    setup project

---
+++ components/quarks/Container.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-full max-w-[1120px] mx-auto">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ components/templates/HomeTemplate.vue
@@ -0,0 +1,9 @@
+<template>
+    <Container>
+        <h2 class="text-2xl text-red-900">Hello World</h2>
+    </Container>
+</template>
+
+<script setup lang="ts">
+
+</script>
