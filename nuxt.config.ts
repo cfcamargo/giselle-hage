790638ea11692e52a7564c8f4e75257eeb81717a
Cristian Camargo <contato@camargodev.com.br>
2024-03-25 20:53:43 -0400

    feat: add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,7 @@ export default defineNuxtConfig({
 			title : 'Giselle Hage | Harmonização Orofacial',
 			meta: [
 				{ hid: 'description', name: 'description', content: '>Olá, sou a Giselle, uma cirurgiã dentista completamente apaixonada pela beleza e tudo que ele representa.Minha verdadeira paixão: a harmonização orofacial. Tenho dedicado toda a minha carreira para unir a arte de transformar auto estima com a magia de promover a harmonia estética em cada expressão facial.' },
+				{ name: 'keywords', content: 'harmonização orofacial, botox, peeling, microagulhamento, tratamento de manchas, saúde, beleza, tratamento estético, estética, mulher, Giselle Hage, pele saudável, cuidados faciais, tramatamento de pele, rejuvenescimento, harmonização facial, harmonização orofacial, esteticista'}
 			]
 		}
 	}
